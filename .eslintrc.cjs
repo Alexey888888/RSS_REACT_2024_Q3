478a2a3e9913a6af00e046bb12bb75a218a95d77
Alexey888888 <119805836+Alexey888888@users.noreply.github.com>
2024-07-03 17:14:12 +0300

    chore: integrate Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,15 +5,13 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', 'react-compiler'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/no-explicit-any': 'error',
     'react-compiler/react-compiler': 'error',
   },
