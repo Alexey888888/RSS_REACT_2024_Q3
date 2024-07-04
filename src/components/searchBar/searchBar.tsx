a4dea64b8da19c9cb602935eae310c691b35a71d
Alexey888888 <119805836+Alexey888888@users.noreply.github.com>
2024-07-04 16:07:46 +0300

    feat: add button component

--- src/components/searchBar/searchBar.tsx
+++ src/components/searchBar/searchBar.tsx
@@ -1,10 +1,12 @@
 import { Component } from 'react';
+import { Button } from '../button/button';
 
 export class SearchBar extends Component {
   render() {
     return (
       <div>
         <h2>SearchBar component</h2>
+        <Button type="submit" text="Search" />
       </div>
     );
   }
