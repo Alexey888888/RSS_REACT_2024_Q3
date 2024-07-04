600a74a756e48aa5fdfa31613cc38d16ed62e6b5
Alexey888888 <119805836+Alexey888888@users.noreply.github.com>
2024-07-04 15:09:37 +0300

    feat: add modern-normalize

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+
+import '../node_modules/modern-normalize/modern-normalize.css';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
