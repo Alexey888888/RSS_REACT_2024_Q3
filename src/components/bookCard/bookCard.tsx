a76b42a137e32f9b685ae6bedc153b6326e3b30d
Alexey888888 <119805836+Alexey888888@users.noreply.github.com>
2024-07-09 14:52:11 +0300

    refactor: refactor BookCard to functional component

--- src/components/bookCard/bookCard.tsx
+++ src/components/bookCard/bookCard.tsx
@@ -1,34 +1,25 @@
-import { Component } from 'react';
 import { IBookCardProps } from './IBookCard';
 
 import './bookCard.scss';
 
-export class BookCard extends Component<IBookCardProps> {
-  constructor(props: IBookCardProps) {
-    super(props);
-  }
-
-  render() {
-    const { title, publishedYear, numberOfPages } = this.props;
-
-    return (
-      <div className="book-card">
-        <div className="book-card__inner">
+export const BookCard: React.FC<IBookCardProps> = ({ title, publishedYear, numberOfPages }) => {
+  return (
+    <div className="book-card">
+      <div className="book-card__inner">
+        <p>
+          <b>Title</b>: {title}
+        </p>
+        {publishedYear && (
           <p>
-            <b>Title</b>: {title}
+            <b>Published year</b>: {publishedYear}
           </p>
-          {publishedYear && (
-            <p>
-              <b>Published year</b>: {publishedYear}
-            </p>
-          )}
-          {numberOfPages && (
-            <p>
-              <b>Number of pages</b>: {numberOfPages}
-            </p>
-          )}
-        </div>
+        )}
+        {numberOfPages && (
+          <p>
+            <b>Number of pages</b>: {numberOfPages}
+          </p>
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
