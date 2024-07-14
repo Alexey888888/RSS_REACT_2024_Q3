cadd10c90f95921e8c3e421613d178e519083bae
Alexey888888 <119805836+Alexey888888@users.noreply.github.com>
2024-07-14 18:06:21 +0300

    chore: install and set vitest and testing-librarys

---
+++ src/components/button/button.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
+import { Button } from './button';
+import { IButtonProps } from './IButton';
+
+describe('Button component', () => {
+  it('renders button with text', () => {
+    const props: IButtonProps = {
+      type: 'button',
+      text: 'Test click',
+      onClick: vi.fn(),
+    };
+
+    render(<Button {...props} />);
+    const buttonElement = screen.getByText(/Test click/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it('calls onClick when clicked', () => {
+    const onClickMock = vi.fn();
+    const props: IButtonProps = {
+      type: 'button',
+      text: 'Test click',
+      onClick: onClickMock,
+    };
+
+    render(<Button {...props} />);
+    const buttonElement = screen.getByText(/Test click/i);
+    fireEvent.click(buttonElement);
+    expect(onClickMock).toHaveBeenCalledTimes(1);
+  });
+});
