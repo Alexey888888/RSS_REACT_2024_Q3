b877eeb1d82aa4b7213c65dfd0dfc49d40661795
Alexey888888 <119805836+Alexey888888@users.noreply.github.com>
2024-07-24 12:23:04 +0300

    feat: add test for ErrorBoundary

--- src/main.test.tsx
+++ src/main.test.tsx
@@ -3,19 +3,26 @@ import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import { App } from './App';
 import { render } from '@testing-library/react';
+import { act } from 'react';
 
 describe('main.tsx', () => {
   it('renders the App without crashing', () => {
     const root = document.createElement('div');
     root.id = 'root';
     document.body.appendChild(root);
-    renderApp(root);
-    const app = render(
-      <Provider store={store}>
-        <App />
-      </Provider>,
-    );
-    expect(app.container).toBeTruthy();
+
+    act(() => {
+      renderApp(root);
+      render(
+        <Provider store={store}>
+          <App />
+        </Provider>,
+      );
+    });
+
+    const app = document.querySelector('#root');
+    expect(app).toBeTruthy();
+
     document.body.removeChild(root);
   });
 });
