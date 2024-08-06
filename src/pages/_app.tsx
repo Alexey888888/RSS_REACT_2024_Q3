9cd17d0af2285d3defa40633f1bda8b6ea406ddd
Alexey888888 <119805836+Alexey888888@users.noreply.github.com>
2024-08-06 13:51:20 +0300

    feat: add modern-normalize.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import { store } from '../redux/store';
 import { ThemeProvider } from '../context/themeContext';
 
+import '../styles/globals.scss';
+import '../../node_modules/modern-normalize/modern-normalize.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
