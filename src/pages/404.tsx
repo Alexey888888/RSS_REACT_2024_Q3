407e4b5d6cd6299a30c23dd4121999d3f4a80fd5
Alexey888888 <119805836+Alexey888888@users.noreply.github.com>
2024-08-06 13:32:12 +0300

    fix: fix 404 page styles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Link from 'next/link';
+
 import styles from '../styles/notFoundPage.module.scss';
+
 const NotFoundPage: React.FC = () => {
   return (
     <div className={styles.notFoundPage}>
-      <h1>
+      <h1 className={styles.notFoundPage__title}>
         404
         <br />
         Page not found
       </h1>
-      <Link className="not-found-page__link" href="/">
+      <Link className={styles.notFoundPage__link} href="/">
         Back to the main page
       </Link>
     </div>
