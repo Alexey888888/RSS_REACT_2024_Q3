a065df1a92577e9e329554eaa9203ab874a5d559
Alexey888888 <119805836+Alexey888888@users.noreply.github.com>
2024-08-07 23:28:45 +0300

    chore: change vitest.config.js

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module 'react-csv' {
-  export const CSVLink: React.FC<{
-    data: object[];
-    filename: string;
-    children?: React.ReactNode;
-  }>;
-}
--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,14 @@ export default defineConfig({
     setupFiles: './src/setupTests.ts',
     coverage: {
       provider: 'v8',
+      exclude: [
+        'src/pages/_document.tsx',
+        'vitest.config.ts',
+        'next-env.d.ts',
+        '**/*.js',
+        '**/*.mjs',
+        '**/*.cjs',
+      ],
     },
   },
 });
