832ee36d9022e67624a1d96057161ffff091da32
Alexey888888 <119805836+Alexey888888@users.noreply.github.com>
2024-08-07 14:18:56 +0300

    feat: add tests for 404 page

---
+++ src/__tests__/404.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import NotFoundPage from '../pages/404';
+import React from 'react';
+
+describe('NotFoundPage Component', () => {
+  it('should render correctly', () => {
+    render(<NotFoundPage />);
+    expect(screen.getByRole('heading', { name: /404/i })).toBeTruthy();
+    expect(screen.getByText('Back to the main page')).toBeTruthy();
+  });
+});
